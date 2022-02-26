dc069e16a12ab2862885de394cedc8ae3ef55ee3
Alejandro Zamora <alejandroo14@gmail.com>
2022-02-26 14:09:21 -0500

    Se crea boilerplate con queries

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="container">
+        <div className="row v-center space-between">
+          <div className="logo">
+            <a href="/">AGNECY.</a>
+          </div>
+          <nav className="nav">
+            <span></span>
+            <span></span>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
